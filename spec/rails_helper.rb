bbef5f807f83e888e24b88ccd5fc8ecc070d0cb2
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-27 17:19:09 -0200

    [#86850260] prevent rspec to show database logs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,10 @@ require Rails.root.join('spec/support/blueprints')
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+ActiveRecord::Base.logger.level = Logger::INFO
+ActionController::Base.logger.level = Logger::ERROR
+Rails.logger.level = Logger::ERROR
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
