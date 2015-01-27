25ef39bdde40aee37ef13a4181ec5de71f3f3057
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-27 17:04:39 -0200

    [#86850260] add first and last names to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127190100) do
+ActiveRecord::Schema.define(version: 20150127190420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20150127190100) do
   create_table "users", force: true do |t|
     t.string "email"
     t.string "avatar"
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
