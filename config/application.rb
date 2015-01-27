8b7ea8b573b19253fb3e928aca646db9dfb163a5
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-27 16:48:18 -0200

    [#86850260] add rack-cas and setup the login integration

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rack-cas/session_store/active_record'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,5 +20,8 @@ module Deolho2
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.rack_cas.server_url = ENV['CAS_SERVER_URL']
+    config.rack_cas.session_store = RackCAS::ActiveRecordStore
   end
 end
