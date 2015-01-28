1337a26c9e67dc9072fd598e61c243d18a379337
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-28 13:31:26 -0200

    [#86850454] add projects_controller#close_for_contribution tests

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, :type => :controller do
-  describe "GET show" do
-    before { @project = Project.make! }
+  before { @project = Project.make! }
 
+  describe "GET show" do
     it "should assigns @project" do
       get :show, id: @project.id
       expect(assigns(:project)).to be_eql(@project)
+    end
+  end
+
+  describe "PUT close_for_contribution" do
+    it "should close the project for contribution" do
+      put :close_for_contribution, id: @project.id
+      expect(@project.reload.closed_for_contribution?).to be_truthy
+    end
+
+    it "should redirects to the project" do
+      put :close_for_contribution, id: @project.id
+      expect(response).to redirect_to(project_path(@project))
     end
   end
 end
