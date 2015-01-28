9d701a047304a9cf2c4f333b01234f2fcf8e4627
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-28 13:23:27 -0200

    [#86850454] add projects_controller#show tests

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, :type => :controller do
+  describe "GET show" do
+    before { @project = Project.make! }
 
+    it "should assigns @project" do
+      get :show, id: @project.id
+      expect(assigns(:project)).to be_eql(@project)
+    end
+  end
 end
