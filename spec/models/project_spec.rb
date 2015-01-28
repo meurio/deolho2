9f3b2142c0af75ad22fef2927c6d3812550c6db7
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-01-28 13:43:19 -0200

    [#86850454] add tests to project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#closed_for_contribution?" do
+    context "when closed_for_contribution_at is nil" do
+      it "should be false" do
+        expect(subject.closed_for_contribution?).to be_falsey
+      end
+    end
+
+    context "when closed_for_contribution_at is not nil" do
+      before { subject.close_for_contribution }
+
+      it "should be true" do
+        expect(subject.closed_for_contribution?).to be_truthy
+      end
+    end
+  end
+
+  describe "#close_for_contribution" do
+    subject { Project.make! }
+
+    it "should update closed_for_contribution_at" do
+      expect {
+        subject.close_for_contribution
+      }.to change{ subject.closed_for_contribution_at }
+    end
+  end
 end
