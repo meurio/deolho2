53e8f705e7d6d206c7dc6f195ffcd3cfabb639d4
Josias Schneider <josias.ds@gmail.com>
2015-01-30 10:18:30 -0200

    [#86850450] add title to Project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129125755) do
+ActiveRecord::Schema.define(version: 20150130115114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150129125755) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "closed_for_contribution_at"
+    t.string   "title"
   end
 
   create_table "sessions", force: :cascade do |t|
