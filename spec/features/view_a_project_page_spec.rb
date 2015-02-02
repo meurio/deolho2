1a83db3bba75cada146daeb25604df62794a43ca
Josias Schneider <josias.ds@gmail.com>
2015-02-02 13:20:14 -0200

    [#86850432] add link to adopt project

--- spec/features/view_a_project_page_spec.rb
+++ spec/features/view_a_project_page_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 RSpec.feature 'View a project page' do
   let(:project) { Project.make! }
+  before { visit project_path(project) }
 
   scenario 'should see the Facebook share button' do
-    visit project_path(project)
     expect(page).to have_css('a.share-on-facebook-button')
   end
 
   scenario 'should see the Twitter share button' do
-    visit project_path(project)
     expect(page).to have_css('a.share-on-twitter-button')
+  end
+
+  scenario 'should see the adopt project button' do
+    expect(page).to have_css('a#adopt-project-button')
+  end
+
+  scenario 'should see the adopt project message', js: true do
+    page.click_link 'adopt-project-button'
+    expect(page).to have_css('#adopt.open')
   end
 end
