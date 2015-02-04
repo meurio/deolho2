ef8a729f185d3e8ad0de0ced923c7d489d0374c0
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-02-03 23:09:01 -0200

    [#86850272] fix carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     config.enable_processing = Rails.env.development?
   else
     config.fog_credentials = {
-      config.provider               = 'AWS'
-      config.aws_access_key_id      = ENV['AWS_ID']
+      config.provider               = 'AWS',
+      config.aws_access_key_id      = ENV['AWS_ID'],
       config.aws_secret_access_key  = ENV['AWS_SECRET']
     }
     config.fog_directory = ENV['AWS_BUCKET']
