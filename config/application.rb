5e50beb49cda930f1220625c2b0efd0b0026b602
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-02-11 11:31:18 -0200

    [#86850264] change app name to Legislando

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rack-cas/session_store/active_record'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Deolho2
+module Legislando
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
