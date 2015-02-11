7b5a8b8bd09e38088ef3355e1a4b1933ff59e9e2
Josias Schneider <josias.ds@gmail.com>
2015-02-11 16:11:42 -0200

    [#86850276] add adopters, contributors and signers count to the project page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,8 @@ class Project < ActiveRecord::Base
   has_many :signers, through: :signatures, source: :user
   has_many :contributions
   has_many :contributors, through: :contributions, source: :user
+  has_many :adoptions
+  has_many :adopters, through: :adoptions, source: :user
   belongs_to :category
   belongs_to :organization
 
