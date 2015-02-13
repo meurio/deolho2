53b293fe503dfbe62f875c63684f304998e184d0
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-02-13 10:30:07 -0200

    [#86850302] add Sendgrid configuration

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,16 @@ module Legislando
     config.rack_cas.session_store = RackCAS::ActiveRecordStore
 
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      :user_name => ENV["SENDGRID_USERNAME"],
+      :password => ENV["SENDGRID_PASSWORD"],
+      :domain => "legislando.nossascidades.org",
+      :address => "smtp.sendgrid.net",
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end
