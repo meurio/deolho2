1db2617f366f1264b79d5b2ffe0bc83948059d1b
Josias Schneider <josias.ds@gmail.com>
2015-02-19 15:21:52 -0200

    [#86850460] add accepted_at and rejected_at fields to project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -33,9 +33,9 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:title, :abstract, :category_id, :organization_id,
-    :google_drive_embed, :google_drive_url, :closes_for_contribution_at, :facebook_share_image,
-    :facebook_share_description, :facebook_share_title, :twitter_share_message, :legislative_chamber,
-    :legislative_processing, :email_to_signer, :email_to_contributor, :taf_message, :image)
+    params.require(:project).permit(:title, :image, :abstract, :category_id, :organization_id,
+      :google_drive_embed, :google_drive_url, :closes_for_contribution_at, :facebook_share_image,
+      :facebook_share_description, :facebook_share_title, :twitter_share_message, :legislative_chamber,
+      :legislative_processing, :email_to_signer, :email_to_contributor, :taf_message, :accepted_at, :rejected_at)
   end
 end
