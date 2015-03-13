f8125f8947fc4b2e8e1429da12a73e45b2184581
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-03-13 10:37:57 -0300

    Add projects/show locale file

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Legislando
     config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = "pt-BR"
 
     config.rack_cas.server_url = ENV['CAS_SERVER_URL']
