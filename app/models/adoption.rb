dce83ec8ad0286be5d116e2d86e3b80e616ea7a2
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-12 10:26:12 -0300

    [#94295548] add users to segments when they adopt a project

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -4,4 +4,17 @@ class Adoption < ActiveRecord::Base
 
   validates :user, :project, presence: true
   validates :user_id, uniqueness: { scope: :project_id }
+
+  after_create { self.delay.create_segment_subscription }
+
+  def create_segment_subscription
+    HTTParty.post(
+      "#{ENV["ACCOUNTS_HOST"]}/users/#{self.user_id}/segment_subscriptions.json",
+      body: {
+        token: ENV["ACCOUNTS_API_TOKEN"],
+        segment_subscription: { segment_id: self.project.mailchimp_segment_uid }
+      }.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
 end
