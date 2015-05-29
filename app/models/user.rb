b4b85a48910041950fffb9c8bffff68eada361de
Josias Schneider <josias.ds@gmail.com>
2015-05-29 10:14:07 -0300

    [#95424856] User has one GoogleAuthorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   acts_as_our_cities_user
 
+  has_one :google_authorization
+
   def signed? project
     Signature.where(user_id: self.id, project_id: project.id).any?
   end
