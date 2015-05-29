33605aae8a38663300c3135d0bc6f0d4a5ab1192
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-29 17:09:18 -0300

    [#95418324] fix GoogleAuthorizationsController#grant action

--- app/controllers/google_authorizations_controller.rb
+++ app/controllers/google_authorizations_controller.rb
@@ -13,6 +13,8 @@ class GoogleAuthorizationsController < ApplicationController
   def grant
     auth = GoogleDrive.generate_authorization
     auth.code = params[:code] if params[:code]
+    auth.redirect_uri = grant_google_authorizations_url
+    auth.grant_type = "authorization_code"
 
     begin
       auth.fetch_access_token!
@@ -22,7 +24,7 @@ class GoogleAuthorizationsController < ApplicationController
 
     ga = GoogleAuthorization.find_or_initialize_by(user: current_user)
     ga.access_token = auth.access_token
-    ga.refresh_token = auth.refresh_token
+    ga.refresh_token = auth.refresh_token if auth.refresh_token.present?
     ga.issued_at = auth.issued_at
     ga.expires_at = ga.issued_at + auth.expires_in
     ga.save!
